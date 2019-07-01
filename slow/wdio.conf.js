199b62e78aefb09526c2d6bd0bb309bb3e7f707c
Tarasov Sergey <tarasov@frankrg.com>
2019-07-01 15:04:20 +0300

    Updated logs.

--- slow/wdio.conf.js
+++ slow/wdio.conf.js
@@ -74,7 +74,7 @@ exports.config = {
     // Define all options that are relevant for the WebdriverIO instance here
     //
     // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: 'error',
+    logLevel: 'trace',
     // Set directory to store all logs into
     outputDir: './logs/',
     //
